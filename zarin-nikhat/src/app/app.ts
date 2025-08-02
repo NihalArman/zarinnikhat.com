b43822aabd100667ef9cf9239a45ada6a0d0b488
Arman Nihal <armansaliknihal@gmail.com>
2025-08-02 12:06:57 +0300

    Add navigation bar at top of the page

--- zarin-nikhat/src/app/app.ts
+++ zarin-nikhat/src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HomePage} from './home-page/home-page';
+import {Navbar} from './navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HomePage],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
