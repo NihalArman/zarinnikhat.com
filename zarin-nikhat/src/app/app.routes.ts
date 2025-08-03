aa2668e634218c403fe5d35dbe2e225e6376034a
Arman Nihal <armansaliknihal@gmail.com>
2025-08-03 12:46:05 +0300

    Add showcase page slot in the navbar

--- zarin-nikhat/src/app/app.routes.ts
+++ zarin-nikhat/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HomePage} from './home-page/home-page';
 import {AboutPage} from './about-page/about-page';
+import {ShowcasePage} from './showcase-page/showcase-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
+  { path: 'showcase', component: ShowcasePage },
   { path: 'about', component: AboutPage },
 ];
 
