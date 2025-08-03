2cc1fe3e04262083b1fafa62f5d9a41354da0272
Arman Nihal <armansaliknihal@gmail.com>
2025-08-03 12:35:25 +0300

    Make navbar collapsable during click in mobile view

--- zarin-nikhat/src/app/navbar/navbar.ts
+++ zarin-nikhat/src/app/navbar/navbar.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import {RouterLink} from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   imports: [
-    RouterLink
+    RouterLink,
+    NgClass
   ],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css'
 })
 export class Navbar {
+  isCollapsed = true;
 
+  toggleNavbar() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  closeNavbar() {
+    this.isCollapsed = true;
+  }
 }
