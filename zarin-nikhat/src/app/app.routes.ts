de833a8ddfb86837086cf76afc00c8256d8ee0c3
Arman Nihal <armansaliknihal@gmail.com>
2025-08-03 12:42:23 +0300

    Remove contact page from Navigation bar

--- zarin-nikhat/src/app/app.routes.ts
+++ zarin-nikhat/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import {HomePage} from './home-page/home-page';
 import {AboutPage} from './about-page/about-page';
-import {ContactPage} from './contact-page/contact-page';
 
 export const routes: Routes = [
   { path: '', component: HomePage },
   { path: 'about', component: AboutPage },
-  { path: 'contact', component: ContactPage },
 ];
 
