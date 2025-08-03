6d9969833b943ef48c24d7bd7b4a7a6a806af0ba
Arman Nihal <armansaliknihal@gmail.com>
2025-08-03 12:43:39 +0300

    Create empty showcase component

---
+++ zarin-nikhat/src/app/showcase-page/showcase-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showcase-page',
+  imports: [],
+  templateUrl: './showcase-page.html',
+  styleUrl: './showcase-page.css'
+})
+export class ShowcasePage {
+
+}
