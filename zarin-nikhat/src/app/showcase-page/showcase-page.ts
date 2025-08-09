ba2fd496b58c05cc9748793bbc8fb305262e4ddc
Arman Nihal <armansaliknihal@gmail.com>
2025-08-09 18:33:18 +0300

    Desgin showcase page

--- zarin-nikhat/src/app/showcase-page/showcase-page.ts
+++ zarin-nikhat/src/app/showcase-page/showcase-page.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
-import {NgOptimizedImage} from '@angular/common';
+import { Component, HostListener } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 @Component({
   selector: 'app-showcase-page',
-  imports: [
-    NgOptimizedImage
-  ],
+  imports: [NgOptimizedImage],
+  standalone: true, // make sure it's standalone if using Angular 17+
   templateUrl: './showcase-page.html',
   styleUrl: './showcase-page.css'
 })
 export class ShowcasePage {
+  clayImages = ['purple-flower.jpeg', 'sandy-beach.jpeg'];
+  digitalPaintings = ['purple-flower.jpeg', 'sandy-beach.jpeg'];
+  acrylicPaintings = ['purple-flower.jpeg', 'sandy-beach.jpeg'];
 
+  selectedImage: string | null = null;
+
+  openImage(imgPath: string) {
+    this.selectedImage = imgPath;
+  }
+
+  closeImage() {
+    this.selectedImage = null;
+  }
+
+  @HostListener('document:keydown.escape')
+  handleEscape() {
+    this.closeImage();
+  }
 }
