fddbff5f69e144a80cc7787c63accf66e4ac7612
Arman Nihal <armansaliknihal@gmail.com>
2025-08-09 19:12:41 +0300

    Add showcase images

--- zarin-nikhat/src/app/showcase-page/showcase-page.ts
+++ zarin-nikhat/src/app/showcase-page/showcase-page.ts
@@ -9,9 +9,9 @@ import { NgOptimizedImage } from '@angular/common';
   styleUrl: './showcase-page.css'
 })
 export class ShowcasePage {
-  clayImages = ['purple-flower.jpeg', 'sandy-beach.jpeg'];
+  clayImages = ['clay1.jpeg', 'clay2.jpeg', 'clay3.jpeg', 'clay4.jpeg', 'clay5.jpeg'];
   digitalPaintings = ['purple-flower.jpeg', 'sandy-beach.jpeg'];
-  acrylicPaintings = ['purple-flower.jpeg', 'sandy-beach.jpeg'];
+  acrylicPaintings = ['acrylicpaint1.jpeg', 'acrylicpaint2.jpeg', 'acrylicpaint3.jpeg', 'acrylicpaint4.jpeg'];
 
   selectedImage: string | null = null;
 
