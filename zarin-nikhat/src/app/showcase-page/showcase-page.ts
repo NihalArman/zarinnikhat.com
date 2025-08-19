1b92271134ff4e120eded64a31de8d940d912344
Arman Nihal <armansaliknihal@gmail.com>
2025-08-19 19:51:51 +0300

    Update showcase clay images

--- zarin-nikhat/src/app/showcase-page/showcase-page.ts
+++ zarin-nikhat/src/app/showcase-page/showcase-page.ts
@@ -9,7 +9,7 @@ import { NgOptimizedImage } from '@angular/common';
   styleUrl: './showcase-page.css'
 })
 export class ShowcasePage {
-  clayImages = ['clay1.jpeg', 'clay2.jpeg', 'clay3.jpeg', 'clay4.jpeg', 'clay5.jpeg'];
+  clayImages = ['clay1.jpeg', 'clay2.jpeg', 'clay3.jpeg', 'clay4.jpeg', 'clay5.jpeg', 'clay6.jpeg', 'clay7.jpeg', 'clay8.jpeg', 'clay9.jpeg', 'clay10.jpeg'];
   digitalPaintings = ['digitalart1.jpeg', 'digitalart2.jpeg', 'digitalart3.jpg', 'digitalart4.jpg', 'digitalart5.jpg', 'digitalart6.jpg', 'digitalart7.jpg', 'digitalart8.jpg', 'digitalart9.jpg', 'digitalart10.jpg', 'digitalart11.jpg'];
   acrylicPaintings = ['acrylicpaint1.jpeg', 'acrylicpaint2.jpeg', 'acrylicpaint3.jpeg', 'acrylicpaint4.jpeg'];
 
